fb7910c76211088f4f09de234fb8728bf1a5acc7
Adam Perry <adam.n.perry@gmail.com>
2015-01-04 19:12:22 -0700

    Successful Hello World!
    Builds to an executable jar properly, answers on 8080 and 8081 when run.

--- src/main/java/io/sirfrancis/bacon/resources/HelloWorldResource.java
+++ src/main/java/io/sirfrancis/bacon/resources/HelloWorldResource.java
@@ -1,6 +1,7 @@
 package io.sirfrancis.bacon.resources;
 
 import com.codahale.metrics.annotation.Timed;
+import com.google.common.base.Optional;
 import io.sirfrancis.bacon.core.Saying;
 
 import javax.ws.rs.GET;
@@ -8,7 +9,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -32,7 +32,7 @@ public class HelloWorldResource {
 	@GET
 	@Timed
 	public Saying sayHello(@QueryParam("name") Optional<String> name) {
-		final String value = String.format(template, name.of(defaultName));
+		final String value = String.format(template, name.or(defaultName));
 		return new Saying(counter.incrementAndGet(), value);
 	}
 }
