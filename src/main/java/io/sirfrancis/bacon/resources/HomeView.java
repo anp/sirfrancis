2eab736af7c4d38731c14e14636d81458f509b45
Adam Perry <adam.n.perry@gmail.com>
2015-01-11 15:55:02 -0700

    Pretty Hello World with OrientDB running in bg
    Using mustache templates for a basic bootstrap helloworld with google
    analytics turned on.

---
+++ src/main/java/io/sirfrancis/bacon/resources/HomeView.java
@@ -0,0 +1,13 @@
+package io.sirfrancis.bacon.resources;
+
+import io.dropwizard.views.View;
+
+/**
+ * Created by Adam on 1/11/2015.
+ */
+public class HomeView extends View {
+
+	public HomeView() {
+		super("home.mustache");
+	}
+}
